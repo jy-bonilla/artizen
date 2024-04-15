dca0ce8490001b3412307b53eb519dd0e1b351c2
jy-bonilla <91812771+jy-bonilla@users.noreply.github.com>
2024-04-14 20:00:55 -0400

    fixed modal layout error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from '@clerk/nextjs';
+
+import { ModalProvider } from "@/providers/modal-provider";
+
 import "./globals.css";
-import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +20,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ModalProvider/>
+        {children}</body>
     </html>
     </ClerkProvider>
   );
